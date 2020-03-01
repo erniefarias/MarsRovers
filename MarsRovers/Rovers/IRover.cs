a21b8b327044909e734e9ca2c59199d0ebd652ea
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 17:03:25 -0500

    Add comments

--- MarsRovers/Rovers/IRover.cs
+++ MarsRovers/Rovers/IRover.cs
@@ -1,13 +1,38 @@
 ﻿namespace MarsRovers.Rovers
 {
+    /// <summary>
+    /// Represents a rover that can move or rotate.
+    /// </summary>
     public interface IRover
     {
+        /// <summary>
+        /// Gets or sets the X coordinate (North/South).
+        /// </summary>
         int XCoordinate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate (West/East).
+        /// </summary>
         int YCoordinate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the direction this rover is facing.
+        /// </summary>
         Direction Direction { get; set; }
 
+        /// <summary>
+        /// Moves the rover forward.
+        /// </summary>
         void MoveForward();
+
+        /// <summary>
+        /// Makes the rover turn left 90 degrees.
+        /// </summary>
         void TurnLeft();
+
+        /// <summary>
+        /// Makes the rover turn right 90 degrees.
+        /// </summary>
         void TurnRight();
     }
 }

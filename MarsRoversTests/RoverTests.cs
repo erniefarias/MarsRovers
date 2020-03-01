20a54e2251d3c3a0002a608971e67ac2d6eef5d8
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 18:21:55 -0500

    Create RoverTests.cs
    Begin with constructor tests

---
+++ MarsRoversTests/RoverTests.cs
@@ -0,0 +1,27 @@
+using MarsRovers.Rovers;
+using Xunit;
+
+namespace MarsRoversTests
+{
+    public class RoverTests
+    {
+        [Fact]
+        public void BaseRoverDefaultValueTest()
+        {
+            // Base rover with no parameters should start at (0,0), facing north
+            var rover = new Rover();
+            Assert.True(rover.XCoordinate == 0 && rover.YCoordinate == 0 &&
+                rover.Direction == MarsRovers.Direction.North);
+        }
+
+        [Fact]
+        public void BaseRoverCoordinateTest()
+        {
+            // Base rover with coordinates should be start with the given
+            // coordinates, facing north
+            var rover = new Rover(5, 10);
+            Assert.True(rover.XCoordinate == 5 && rover.YCoordinate == 10 &&
+                rover.Direction == MarsRovers.Direction.North);
+        }
+    }
+}

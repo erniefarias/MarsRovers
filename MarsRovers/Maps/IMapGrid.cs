dc5b13427637bd91a1600b64737f5b1d0db8489d
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 17:32:39 -0500

    Add comments

--- MarsRovers/Maps/IMapGrid.cs
+++ MarsRovers/Maps/IMapGrid.cs
@@ -1,9 +1,26 @@
 ﻿namespace MarsRovers.Maps
 {
+    /// <summary>
+    /// Represents a map grid with boundaries.
+    /// </summary>
     public interface IMapGrid
     {
+        /// <summary>
+        /// Gets this map's width.
+        /// </summary>
         int Width { get; }
+
+        /// <summary>
+        /// Gets this map's height.
+        /// </summary>
         int Height { get; }
+
+        /// <summary>
+        /// Indicates whether the given coordinate point is within this map's
+        /// boundaries.
+        /// </summary>
+        /// <param name="X">X coordinate</param>
+        /// <param name="Y">Y coordinate</param>
         bool ContainsPoint(int X, int Y);
     }
 }

824f9ff050a19e4cc884ae24a9209f06f301c91e
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 17:02:50 -0500

    Create IRover.cs

---
+++ MarsRovers/Rovers/IRover.cs
@@ -0,0 +1,13 @@
+﻿namespace MarsRovers.Rovers
+{
+    public interface IRover
+    {
+        int XCoordinate { get; set; }
+        int YCoordinate { get; set; }
+        Direction Direction { get; set; }
+
+        void MoveForward();
+        void TurnLeft();
+        void TurnRight();
+    }
+}

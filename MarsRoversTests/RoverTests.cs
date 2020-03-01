617b98559a37937436ef3d0b2ac1d8a370147664
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 18:34:05 -0500

    Add tests

--- MarsRoversTests/RoverTests.cs
+++ MarsRoversTests/RoverTests.cs
@@ -1,3 +1,4 @@
+using MarsRovers;
 using MarsRovers.Rovers;
 using Xunit;
 
@@ -6,22 +7,53 @@ namespace MarsRoversTests
     public class RoverTests
     {
         [Fact]
-        public void BaseRoverDefaultValueTest()
+        public void NoParameterInitializeTest()
         {
             // Base rover with no parameters should start at (0,0), facing north
             var rover = new Rover();
             Assert.True(rover.XCoordinate == 0 && rover.YCoordinate == 0 &&
-                rover.Direction == MarsRovers.Direction.North);
+                rover.Direction == Direction.North);
         }
 
         [Fact]
-        public void BaseRoverCoordinateTest()
+        public void CoordinateAndDirectionInitializeTest()
         {
-            // Base rover with coordinates should start with the given
-            // coordinates, facing north
-            var rover = new Rover(5, 10);
-            Assert.True(rover.XCoordinate == 5 && rover.YCoordinate == 10 &&
-                rover.Direction == MarsRovers.Direction.North);
+            // Base rover with coordinates and direction should start with the
+            // given coordinates and direction
+            var rover = new Rover(6, 12, Direction.West);
+            Assert.True(rover.XCoordinate == 6 && rover.YCoordinate == 12 &&
+                rover.Direction == Direction.West);
+        }
+
+        [Fact]
+        public void TurnLeftTest()
+        {
+            var rover = new Rover();
+            rover.Direction = Direction.West;
+            rover.TurnLeft();
+            Assert.True(rover.Direction == Direction.South);
+        }
+
+        [Fact]
+        public void TurnRightTest()
+        {
+            var rover = new Rover();
+            rover.TurnRight();
+            rover.TurnRight();
+            rover.TurnRight();
+            Assert.True(rover.Direction == Direction.West);
+        }
+
+        [Fact]
+        public void MoveFowardAndTurnRightTest()
+        {
+            var rover = new Rover();
+            rover.MoveForward();
+            rover.MoveForward();
+            rover.TurnRight();
+            rover.MoveForward();
+            Assert.True(rover.XCoordinate == 1 && rover.YCoordinate == 2 &&
+                rover.Direction == Direction.East);
         }
     }
 }

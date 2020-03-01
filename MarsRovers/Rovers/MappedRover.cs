557fd4bbffddb4a3f857082cb6c854ebfd57d730
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 17:56:37 -0500

    Add comments
    Also updated exception for a more descriptive type

--- MarsRovers/Rovers/MappedRover.cs
+++ MarsRovers/Rovers/MappedRover.cs
@@ -3,23 +3,44 @@ using MarsRovers.Maps;
 
 namespace MarsRovers.Rovers
 {
+    /// <summary>
+    /// Represents a rover that is positioned on a map
+    /// </summary>
     public class MappedRover : Rover
     {
+        /// <summary>
+        /// The instance of a map that this rover is moving on.
+        /// </summary>
         private IMapGrid _map;
 
+        /// <summary>
+        /// Initializes an instance of the <see cref="MappedRover"/> class on
+        /// the given map. The rover will start at the southwest corner of the
+        /// map.
+        /// </summary>
+        /// <param name="map">Map to place rover on</param>
         public MappedRover(IMapGrid map)
         {
             _map = map;
         }
 
+        /// <summary>
+        /// Moves the rover forward, when possible.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
         public override void MoveForward()
         {
             if (CanMoveForward())
                 base.MoveForward();
             else
-                throw new Exception("Cannot move forward due to map boundaries");
+                throw new InvalidOperationException("Rover cannot move forward" +
+                    " due to the boundaries on its map.");
         }
 
+        /// <summary>
+        /// Indicates whether the rover can move forward without crossing the
+        /// map's boundaries.
+        /// </summary>
         public bool CanMoveForward()
         {
             int newX = XCoordinate;

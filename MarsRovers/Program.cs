284a064b11c7a2c610a4f9241fcf3c22653db469
Ernie Farias <34193363+erniefarias@users.noreply.github.com>
2020-03-01 21:03:18 -0500

    Add logic to read input file

--- MarsRovers/Program.cs
+++ MarsRovers/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using MarsRovers.Maps;
+using MarsRovers.Rovers;
 
 namespace MarsRovers
 {
@@ -6,7 +9,38 @@ namespace MarsRovers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + "Input.txt";
+
+            try
+            {
+                var fileLines = File.ReadLines(inputFile).GetEnumerator();
+
+                // Use the first line to create the map
+                fileLines.MoveNext();
+                MapGrid map = InputHandler.CreateMap(fileLines.Current);
+
+                MappedRover rover = null;
+
+                while (fileLines.MoveNext())
+                {
+                    string input = fileLines.Current;
+
+                    if (rover == null)
+                    {
+                        rover = InputHandler.CreateRover(map, input);
+                    }
+                    else
+                    {
+                        string status = InputHandler.CommandRover(rover, input);
+                        Console.WriteLine(status);
+                        rover = null;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Input file could not be processed: {e.Message}");
+            }
         }
     }
 }
